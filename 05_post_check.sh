386a617cce2cba297ce0fefb406c06a63ff079b3
Joshua Tigno <tignojoshua@plmun.edu.ph>
2025-02-04 10:06:18 +0800

    Initial Project Files

--- 05_post_check.sh
+++ 05_post_check.sh
@@ -2,20 +2,12 @@
 
 # Database credentials (environment variables are recommended)
 DB_HOST="${DB_HOST:-127.0.0.1}"
-DB_PORT="${DB_PORT:-3306}" # MySQL default port is 3306, not 4000 unless you changed it
+DB_PORT="${DB_PORT:-4000}" # MySQL default port is 3306, not 4000 unless you changed it
 DB_USER="${DB_USER:-root}"
 DB_NAME="${DB_USER:-UniversityDB}"          # Must be set as an environment variable!
 
 # Expected table structure (adjust as needed)
-EXPECTED_STRUCTURE="
-CREATE TABLE `Students` (
-    `StudentID` INT PRIMARY KEY AUTO_INCREMENT,
-    `FirstName` VARCHAR(50) NOT NULL,
-    `LastName` VARCHAR(50) NOT NULL,
-    `EnrollmentDate` DATE,
-    `Email` VARCHAR(100)
-);
-"
+EXPECTED_STRUCTURE=$(cat output/table_structure.txt)
 
 # Function to get the actual table structure from the database
 get_table_structure() {
